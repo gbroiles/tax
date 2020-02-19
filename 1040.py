2ee8e2a4956f815a8813346709018a84f4ea8a82
gbroiles <gbroiles@gmail.com>
2020-02-19 13:10:26 -0800

    use black so it's less ugly

--- 1040.py
+++ 1040.py
@@ -1,19 +1,23 @@
 #! /usr/bin/env python3
-#pylint: disable=missing-module-docstring, missing-function-docstring
+# pylint: disable=missing-module-docstring, missing-function-docstring
 import datetime
 import argparse
 import sys
 import t1040
 
+
 def create_parse():
-    parser = argparse.ArgumentParser(
-        description='US individual tax calculator')
-    parser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
+    parser = argparse.ArgumentParser(description="US individual tax calculator")
+    parser.add_argument(
+        "-v", "--verbose", help="increase verbosity", action="store_true"
+    )
     return parser
+
 
 def log(msg):
     t = datetime.datetime()
     print(t.isoformat(), ": ", msg, file=sys.stderr)
+
 
 def start():
     parser = create_parse()
@@ -26,5 +30,6 @@ def start():
     if verbose:
         log("Program started.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start()
